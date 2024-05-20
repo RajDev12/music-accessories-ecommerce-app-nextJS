4da016df554c3caf0dba451ca861e32b8631e9db
RajDev12 <rjnath98@gmail.com>
2024-05-20 21:50:58 +0530

    update app with stripe

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
-import "@/styles/globals.css";
-import { Layout } from "./components";
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
 
 export default function App({ Component, pageProps }) {
   return(
+    <StateContext>
     <Layout>
-   <Component {...pageProps} />
-   </Layout>
+      <Toaster />
+      <Component {...pageProps} />
+    </Layout>
+  </StateContext>
   )
 }
